7156773d409858e43536f0e7366764e08a7089ee
106010137 <Barsikyanvlad57728@gmail.com>
2024-10-30 12:33:28 +0300

    main branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main branch");
+        System.out.println("Barsikyan_branch");
 
 
 
 
-        
+
 
     }
 }
