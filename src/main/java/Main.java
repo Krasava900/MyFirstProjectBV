e1c83b5a4d9ff68c0910e23686be9c68511cdaae
106010137 <Barsikyanvlad57728@gmail.com>
2024-10-30 12:54:41 +0300

    main branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Enter num: ");
         int enterNum = new Scanner(System.in).nextInt();
         for(int i = 1; i <= enterNum; i++){
             for (int j = 1; j <= enterNum; j++){
