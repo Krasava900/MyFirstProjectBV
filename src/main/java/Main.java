af7f6eb6e4195690e7d54f97b2dfafb89fe0b189
106010137 <Barsikyanvlad57728@gmail.com>
2024-10-30 12:53:51 +0300

    main branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Barsikyan_branch");
-
-
-
-
+        int enterNum = new Scanner(System.in).nextInt();
+        for(int i = 1; i <= enterNum; i++){
+            for (int j = 1; j <= enterNum; j++){
+                if(i * j == enterNum){
+                    System.out.println(i + " * " + j + " = " + enterNum);
+                }
+            }
+        }
 
 
     }
